3cf44de87f87fedae4c975e6c852c467e6668f09
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-22 19:13:58 +0800

    Added some more Model data

--- CalendarApp/CalendarApp/Model/CurrentWeatherModel.cs
+++ CalendarApp/CalendarApp/Model/CurrentWeatherModel.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace CalendarApp.Model
 {
-    public class CurrentWeatherModel
+    public class CurrentWeatherModel : INotifyPropertyChanged
     {
+        public string Cloud { get; set; }
         public string Description { get; set; }
-        public string Icon { get; set; }
         public string Temperature { get; set; }
         public string Feels_like { get; set; }
         public string Temperature_Min { get; set; }
@@ -15,5 +17,14 @@ namespace CalendarApp.Model
         public string Pressure { get; set; }
         public string Humidity { get; set; }
         public string Visibilty { get; set; }
+        public string Wind_Speed { get; set; }
+        public string Wind_Degree { get; set; }
+        public string SunRise { get; set; }
+        public string SunSet { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
+
+    
+
 }
