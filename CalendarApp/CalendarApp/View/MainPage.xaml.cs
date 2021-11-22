d7c710c3dd53cb89888e1f8069690dfc4d5439fb
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-22 19:15:38 +0800

    Added  clearListViewBackground

--- CalendarApp/CalendarApp/View/MainPage.xaml.cs
+++ CalendarApp/CalendarApp/View/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -16,6 +17,7 @@ namespace CalendarApp.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<CurrentWeatherModel> weatherModels = new ObservableCollection<CurrentWeatherModel>();
         public MainPage()
         {
             InitializeComponent();
@@ -28,28 +30,31 @@ namespace CalendarApp.View
             var client = new HttpClient();
             var response = await client.GetStringAsync(currentWeather);
             var jsn = JObject.Parse(response);
-            var description = jsn["weather"][0]["description"];
+            var description = jsn["weather"][0]["description"].ToString();
             var imageIcon = jsn["weather"][0]["icon"].ToString();
             var imageparsed = imageIcon + ".png";
             var temperature = jsn["main"]["temp"];
             var actual_feel = jsn["main"]["feels_like"];
             var min_temp = jsn["main"]["temp_min"];
             var max_temp = jsn["main"]["temp_max"];
-            var pressure = jsn["main"]["pressure"];
-            var humidity = jsn["main"]["humidity"];
-            var visibility = jsn["visibility"];
-            var wind_speed = jsn["wind"]["speed"];
-            var wind_deg = jsn["wind"]["deg"];
-            var wind_gust = jsn["wind"]["gust"];
-            var cloud = jsn["clouds"]["all"];
+            var pressure = jsn["main"]["pressure"].ToString();
+            var formattedPressure = pressure + "psi";
+            var humidity = jsn["main"]["humidity"].ToString();
+            var visibility = jsn["visibility"].ToString();
+            var wind_speed = jsn["wind"]["speed"].ToString();
+            var wind_deg = jsn["wind"]["deg"].ToString();
+            var cloud = jsn["clouds"]["all"].ToString();
             var sunrise = jsn["sys"]["sunrise"];
             var sunset = jsn["sys"]["sunset"];
+            var timeToday = jsn["dt"];
 
             //Time Conversion
             TimeSpan time_sunrise = TimeSpan.FromSeconds((double)sunrise);
             TimeSpan time_sunset = TimeSpan.FromSeconds((double)sunset);
+            TimeSpan time_right_now = TimeSpan.FromSeconds((double)timeToday);
             string sunrise_time = time_sunrise.ToString(@"hh\:mm\:ss");
             string sunset_time = time_sunset.ToString(@"hh\:mm\:ss");
+            string phTime = time_right_now.ToString(@"hh\:mm\:ss");
 
 
             //Computation From Kelvin to Farenheit
@@ -62,10 +67,8 @@ namespace CalendarApp.View
             double cels;
             cels = (faren - 32) * 0.55556;
             var formatedVal = String.Format("{0:0.##}", cels);
-
-            WeatherDesc.Text = description.ToString();
-            ImgThumbNail.Source = imageparsed;
-            TempDesc.Text = formatedVal.ToString() + "°C";
+            var tempFormatedVal = formatedVal + "°C";
+           
 
             //Computation for Feels Like (Kelvin to Frenheit)
             double feelsLike_Faren;
@@ -77,8 +80,8 @@ namespace CalendarApp.View
             double FeelsLike_cels;
             FeelsLike_cels = (feelsLike_Faren - 32) * 0.55556;
             var formatedVal_FeelsLike = String.Format("{0:0.##}", FeelsLike_cels);
-            FeelsLikeContainer.Text = formatedVal_FeelsLike.ToString() + "°C";
-
+            var FeelsLike_Formated = formatedVal_FeelsLike + "°C";
+            
             //Computation From Kelvin to Frenheit(Temp Min)
             double TempMin_Faren;
             double TempMin_constVal = 273;
@@ -89,7 +92,8 @@ namespace CalendarApp.View
             double TempMin_cels;
             TempMin_cels = (TempMin_Faren - 32) * 0.55556;
             var formatedVal_TempMin = String.Format("{0:0.##}", FeelsLike_cels);
-            MinTempContainer.Text = formatedVal_TempMin.ToString() + "°C";
+            var formatedTempMin = formatedVal_TempMin + "°C";
+            
 
             //Computation From Kelvin to Frenheit(Temp Max)
             double TempMax_Faren;
@@ -101,13 +105,42 @@ namespace CalendarApp.View
             double TempMax_cels;
             TempMax_cels = (TempMax_Faren - 32) * 0.55556;
             var formatedVal_TempMax = String.Format("{0:0.##}", FeelsLike_cels);
-            MaxTempContainer.Text = formatedVal_TempMax.ToString() + "°C";
+            var formatedTempMax = formatedVal_TempMax + "°C";
+            
+
+            var WeatherDataToday = new CurrentWeatherModel
+            {
+                Cloud = cloud,
+                Description = description,
+                Temperature = tempFormatedVal,
+                Feels_like = FeelsLike_Formated,
+                Temperature_Min = formatedTempMin,
+                Temperature_Max = formatedTempMax,
+                Pressure = formattedPressure,
+                Humidity = humidity,
+                Visibilty = visibility,
+                Wind_Speed = wind_speed,
+                Wind_Degree = wind_deg,
+                SunRise = sunrise_time,
+                SunSet = sunset_time
+            };
+
+            weatherModels.Add(WeatherDataToday);
+
+            WeatherTodayListView.ItemsSource = weatherModels;
+            ActualTime.Text = phTime;
         }
 
         public string GetForecastData()
         {
             var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=New%20York&appid=12c85f36e6495266648068b2ff1fbde3&cnt=30";
             return forecastURL;
+        }
+
+        private void clearListViewBackground(object sender, ItemTappedEventArgs e)
+        {
+            //created this to avoid the orange color background inside ListView when tapping ListView Item
+            ((ListView)sender).SelectedItem = null;
         }
     }
 }
