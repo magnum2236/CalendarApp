fbe7b3330aeb22b296828e120ffcb0e8fcfcbd74
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-23 18:09:46 +0800

    Model for WeatherSummaryModel

---
+++ CalendarApp/CalendarApp/Model/WeatherSummaryModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CalendarApp.Model
+{
+    public class WeatherSummaryModel : INotifyPropertyChanged
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Temp { get; set; }
+        public string Feels_like { get; set; }
+        public string Temp_min { get; set; }
+        public string Temp_max { get; set; }
+        public string Pressure { get; set; }
+        public int Sea_level { get; set; }
+        public int Grnd_level { get; set; }
+        public int Humidity { get; set; }
+        public double Temp_kf { get; set; }
+        public string Speed { get; set; }
+        public string Deg { get; set; }
+        public string Gust { get; set; }
+        public string All { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class RootObj
+    {
+        public string objectType { get; set; }
+        public List<WeatherSummaryModel> weatherSummaries { get; set; }
+    }
+}
