53bfc92867d94867307371ee51471cd9007234d8
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-23 18:09:23 +0800

    Model for Weather

---
+++ CalendarApp/CalendarApp/Model/WeatherModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarApp.Model
+{
+    public class WeatherModel
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+}
