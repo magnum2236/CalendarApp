4d74ed06fd829c18333926df0e65c332cd5d865b
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-23 18:08:58 +0800

    Model for Cloud

---
+++ CalendarApp/CalendarApp/Model/CloudModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarApp.Model
+{
+    public  class CloudModel
+    {
+        public string all { get; set; }
+    }
+}
