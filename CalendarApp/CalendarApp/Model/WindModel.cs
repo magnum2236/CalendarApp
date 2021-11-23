32afcefa07da5c3c6fd313d0590be826c6fdbff6
Kyle Marc Oliveros <kylemarc2236@gmail.com>
2021-11-23 18:10:45 +0800

    Model for Wind

---
+++ CalendarApp/CalendarApp/Model/WindModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarApp.Model
+{
+    public class WindModel
+    {
+        public string Speed { get; set; }
+        public string Deg { get; set; }
+        public string Gust { get; set; }
+    }
+}
